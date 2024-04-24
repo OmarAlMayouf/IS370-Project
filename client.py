a3fa94f7e56c6cec1d357ee22bf2500d26216a58
OmarAlMayouf <omarmyf2003@gmail.com>
2024-04-24 11:59:09 +0300

    typo

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 import socket
 
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 12345
+host = '127.0.0.1' # client address
+port = 12345 # client port
 
 def main():
     try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket
-        client_socket.connect((SERVER_ADDRESS, SERVER_PORT)) #connecting to the server
-        print(f"Connected to server at {SERVER_ADDRESS}:{SERVER_PORT}")
-        client_socket.close()
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket
+        soc.connect((host, port)) #connecting to the server
+        print(f"Connected to server at {host}:{port}")
+        soc.close()
     except Exception as e:
         print("An error occurred:", e)
 
--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import socket
 
-SERVER_ADDRESS = '127.0.0.1'
-SERVER_PORT = 12345
+host = '127.0.0.1' # server address
+port = 12345 # server port
 
 def main():
     
     try:
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket
-        server_socket.bind((SERVER_ADDRESS, SERVER_PORT)) #mapping the address and port to the server
-        server_socket.listen(5) #listen for connection
-        print(f"Server listening on {SERVER_ADDRESS}:{SERVER_PORT}")
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket
+        soc.bind((host, port)) #mapping the address and port to the server
+        soc.listen(5) #listen for connection
+        print(f"Server listening on {host}:{port}")
         
         while True:
-            client_socket, client_address = server_socket.accept() #Accept connection
+            client_socket, client_address = soc.accept() #Accept connection
             print(f"Connection from {client_address}")
             #TODO Here later @Omar. for now the connection is kept on.
             client_socket.close()
@@ -21,7 +21,7 @@ def main():
         print("An error occurred:", e)
         
     finally:
-        server_socket.close()
+        soc.close()
 
 if __name__ == "__main__":
     main()
